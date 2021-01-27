a051630db69e73f7711d808e0a7e56f5485f310e
chatterjee__ <48859001+ArpitaChatterjee@users.noreply.github.com>
2021-01-27 11:54:03 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,34 +55,6 @@ const responsive = {
     }
     // Carousal
  
-    $(document).ready(function() {
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        margin: 10,
-
-        animateOut: 'fadeOut',
-        autoplay: true,
-        autoplayHoverPause: true,
-        autoplayTimeout: 3000,
-        dots: false,
-        nav: true,
-        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
-        responsive: {
-            0: {
-                items: 1
-            },
-
-            600: {
-                items: 2
-            },
-            1000: {
-                items: 3
-            }
-        }
-    }),
-});
-    
-
 //*===== SCROLL SECTIONS ACTIVE LINK =====*/
 const sections = document.querySelectorAll('section[id]')
 
